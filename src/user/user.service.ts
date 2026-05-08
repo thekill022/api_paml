1d7825e087c8a0a393268cdb1cb6f331888e641f
thekill022 <gibfathoni@gmail.com>
2026-05-09 01:21:11 +0700

    membuat method CRUDS untuk user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Like, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository : Repository<User>
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = this.userRepository.create(createUserDto);
+    return this.userRepository.save(newUser)
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const user =  this.userRepository.findOne({where : {id : id}});
+
+    if(!user) {
+      throw new NotFoundException("User tidak ditemukan");
+    }
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    this.userRepository.update(id, updateUserDto);
+
+    const newData = this.userRepository.findOne({where : {id}});
+
+    if (!newData) {
+      throw new NotFoundException("User tidak ditemukan");
+    }
+
+    return newData;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    this.userRepository.delete(id);
+    return {
+      message : "Berhasil menghapus data user"
+    }
   }
+
+  search(nama : string) {
+    const userData = this.userRepository.find({where : {
+      firstName : Like(nama),
+      lastName : Like(nama)
+    }})
+
+    if(!userData) {
+      throw new NotFoundException("Hasil tidak ditemukan");
+    }
+
+    return userData;
+
+  }
+
 }
