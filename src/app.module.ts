fa297450e509d69f546213467381b6a4aa568a84
thekill022 <gibfathoni@gmail.com>
2026-05-08 23:37:00 +0700

    menambahkan koneksi ke database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath : "../.env"
-  })],
+  }),
+  TypeOrmModule.forRoot({
+    type : 'mysql',
+    host : process.env.HOST,
+    port : Number(process.env.DB_PORT),
+    username : process.env.USER,
+    password : process.env.PASS,
+    database : process.env.DB,
+    entities : [],
+    synchronize : true
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
