0fc3f870b07f8b109b2d6428fac92edfb2ed6f26
thekill022 <gibfathoni@gmail.com>
2026-05-09 00:49:41 +0700

    membuat user dto untuk register

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail({}, {message : "Format email salah"})
+    email! : string;
+    
+    @MinLength(3, {message : "Nama pertama minimal 3 karakter"})
+    @IsString()
+    firstName! : string;
+
+    @IsString()
+    lastName! : string;
+
+    @IsString()
+    @MinLength(8, {message : "Minimal panjang password adalah 8 karakter"})
+    password! : string;
+}
