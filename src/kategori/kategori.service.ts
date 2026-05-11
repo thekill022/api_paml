e05bab9a63b04a5b258043cd960dd56a8de87463
thekill022 <gibfathoni@gmail.com>
2026-05-12 03:16:04 +0700

    membuat service untuk cruds kategori

---
+++ src/kategori/kategori.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateKategoriDto } from './dto/create-kategori.dto';
+import { UpdateKategoriDto } from './dto/update-kategori.dto';
+import { Like, Repository } from 'typeorm';
+import { Kategori } from './entities/kategori.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class KategoriService {
+  constructor(
+    @InjectRepository(Kategori)
+    private readonly kategoriRepository : Repository<Kategori>
+  ) {}
+
+  create(createKategoriDto: CreateKategoriDto) {
+    const kategori = this.kategoriRepository.create(createKategoriDto);
+    this.kategoriRepository.save(kategori);
+  }
+
+  findAll() {
+    const kategori =  this.kategoriRepository.find();
+    
+    if (!kategori) {
+      throw new NotFoundException("Data tidak ditemukan");
+    }
+    return kategori;
+  }
+
+  findOne(id: number) {
+    const kategori =  this.kategoriRepository.findOne({where : {id}})
+
+    if(!kategori) {
+      throw new NotFoundException("Data tidak ditemukan");
+    }
+    return kategori;
+  }
+
+  findByName(nama : string) {
+    const kategori = this.kategoriRepository.findOne({where : {kategori : Like(nama)}});
+
+    if (!kategori) {
+      throw new NotFoundException("Data tidak ditemukan");
+    }
+
+    return kategori;
+  }
+
+  update(id: number, updateKategoriDto: UpdateKategoriDto) {
+      this.update(id, updateKategoriDto);
+
+      const kategori = this.kategoriRepository.findOne({where : {id}});
+      if(!kategori) {
+        throw new NotFoundException("Data tidak ditemukan");
+      }
+      return kategori;
+  }
+
+  remove(id: number) {
+    this.kategoriRepository.delete(id);
+    return {
+      message : "Berhasil menghapus user dengan id " + id
+    }
+  }
+}
