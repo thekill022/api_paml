753f57066992e1d3c42c119036ebb65298f5ba85
thekill022 <gibfathoni@gmail.com>
2026-05-14 16:01:36 +0700

    perbaikan penerapan provider untuk guard

--- src/kategori/kategori.module.ts
+++ src/kategori/kategori.module.ts
@@ -3,15 +3,11 @@ import { KategoriService } from './kategori.service';
 import { KategoriController } from './kategori.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Kategori } from './entities/kategori.entity';
-import { APP_GUARD } from '@nestjs/core';
 import { KategoriGuard } from './role-kategori.guard';
 
 @Module({
   imports : [TypeOrmModule.forFeature([Kategori])],
   controllers: [KategoriController],
-  providers: [KategoriService, {
-    provide : APP_GUARD,
-    useClass : KategoriGuard
-  }],
+  providers: [KategoriService, KategoriGuard],
 })
 export class KategoriModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,16 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { APP_GUARD } from '@nestjs/core';
 import { RoleGuard } from './role/role.guard';
 
 @Module({
   imports : [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, {
-    provide : APP_GUARD,
-    useClass : RoleGuard
-  }],
+  providers: [UserService, RoleGuard],
   exports : [UserService]
 })
 export class UserModule {}
