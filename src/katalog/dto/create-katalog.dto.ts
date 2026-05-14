f678e8216be45edac5278493363cc5c014d26b01
thekill022 <gibfathoni@gmail.com>
2026-05-14 17:00:17 +0700

    perbaikan untuk parse tipe data otomatis dari request di frontend

--- src/katalog/dto/create-katalog.dto.ts
+++ src/katalog/dto/create-katalog.dto.ts
@@ -1,12 +1,22 @@
-import { IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
+import { Transform, Type } from "class-transformer";
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
 
 export class CreateKatalogDto {
     @IsString()
     @MinLength(3, {message : "Minimal nama adalah 3 karakter"})
     nama! : string;
+
+    @Type(() => Number)
     @IsNumber()
     harga! : number;
+
+    @IsOptional()
+    @Transform(({ value }) => value === true || value === 'true')
+    @IsBoolean()
     status? : boolean;
+
+    @Type(() => Number)
+    @IsNumber()
     @IsNotEmpty()
     kategoriId! : number;
 }
